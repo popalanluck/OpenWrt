e7cf563e23a7a206326f4ae16f48def1abf2a6b9
Alan <81753503+popalanluck@users.noreply.github.com>
2023-06-06 11:55:51 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -18,8 +18,7 @@ sed -i 's/PATCHVER:=5.15/PATCHVER:=6.1/g' target/linux/ipq807x/Makefile
 sed -i 's/KERNEL_TESTING_PATCHVER:=6.1/KERNEL_TESTING_PATCHVER:=5.15/g' target/linux/ipq807x/Makefile
 
 # 修改默认IP
-sed -i 's/192.168.1.1/192.168.36.1/g' package/base-files/files/bin/config_generate
-sed -i 's/192.168.6.1/192.168.36.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 
 # 更改默认 Shell 为 zsh
 # sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
